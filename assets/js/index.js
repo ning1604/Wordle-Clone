d33118390cfe6cd0867a79102b632fbe0e5f6de7
Ning <100749559+ning1604@users.noreply.github.com>
2023-05-26 21:05:55 +1000

    added logic to display right/wrong letters and styling to tiles

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,6 +28,79 @@ function isLetter(letter) {
     return /^[A-Za-z]$/.test(letter);
 };
 
+// Compares user input to word of the day, and handles win/lose conditions
+function checkWord(word1, word2) {
+    setTimeout(function () {
+        if (word1 === word2) {
+            return alert('you win! :)');
+        } else if (currTile === 30) {
+            return alert('you lose, the word was ' + word2 + '.');
+        };
+    }, 100);
+};
+
+// Handle the guess's correct letter in the correct space (the green squares)
+function greenLetter(word) {
+    for (let i = 0; i < 5; i++) {
+        if (word[i] === wordOfDay[i]) {
+            let greenTile = currTile - (5 - i)
+            document.getElementById('letter-' + greenTile).classList.add('green');
+        }
+    }
+};
+
+// Handle the yellow letters (correct letters but wrong space)
+function yellowLetter(word) {
+    let yellowLetters = [];
+
+    for (let i = 0; i < 5; i++) {
+        // yellowTile gives position of tile to be changed to yellow
+        let yellowTile = currTile - (5 - word.indexOf(word[i]));
+
+        if (wordOfDay.includes(word[i]) && !yellowLetters.includes(`${word[i]}:${yellowTile}`)) {
+            yellowLetters.push(`${word[i]}:${yellowTile}`)
+            ifGreenTile(word[i])
+            addTileColor(yellowTile, word[i])
+        };
+    };
+    console.log(yellowLetters);
+};
+
+// Handle color of tile
+function addTileColor(tileIndex, letter) {
+    let elementOne = document.getElementById('letter-' + tileIndex).classList;
+    if (elementOne.contains('green') || ifGreenTile(letter)) {
+        return
+    } else {
+        elementOne.add('yellow');
+    }
+};
+
+// Checks if current word already has green tile, if so return true
+function ifGreenTile(letter) {
+    let result = false;
+    for (let j = 0; j < 5; j++) {
+        let elementTwo = document.getElementById('letter-' + (currTile - (5 - j)));
+        
+        if (elementTwo.classList.contains('green') && elementTwo.innerHTML == letter) {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+// Changes tile color to grey if letter is not in word
+function greyLetter() {
+    for (let k = 0; k < 5; k++) {
+        let greyTile = document.getElementById('letter-' + (currTile - (5 - k)));
+        
+        if (!greyTile.classList.contains('green') && !greyTile.classList.contains('yellow')) {
+            greyTile.classList.add('grey');
+        }
+    }
+}
+
 // Handle keystroke
 document.addEventListener('keydown', (event) => {
     if (isLetter(event.key) && word.length < 5) {
@@ -41,6 +114,10 @@ document.addEventListener('keydown', (event) => {
         document.getElementById('letter-' + currTile).innerHTML = '';
         word = word.slice(0, -1);
     } else if (event.key === 'Enter' && word.length === 5) {
+        greenLetter(word);
+        yellowLetter(word);
+        greyLetter();
+        checkWord(word, wordOfDay);
         // Move to the next word if Enter is pressed and word length is 5
         prevTile += 5;
         word = '';
